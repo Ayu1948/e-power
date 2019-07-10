e297c14537a2c882e82cc35c8d6364d94d84f473
zht <Ayu1948@163.com>
2019-07-10 09:55:43 +0800

    实现视频播放时定点暂停和继续播放

--- src/app/scene/scene.page.ts
+++ src/app/scene/scene.page.ts
@@ -1,6 +1,6 @@
 // import { VideoPlayer } from '@ionic-native/video-player/ngx';
 import { Component } from '@angular/core';
-import {VgAPI} from 'videogular2/core';
+import { VgAPI } from 'videogular2/core';
 
 @Component({
   selector: 'app-scene',
@@ -8,20 +8,35 @@ import {VgAPI} from 'videogular2/core';
   styleUrls: ['scene.page.scss']
 })
 export class ScenePage {
-  preload:string = 'auto';
-  api:VgAPI;
+  preload = 'auto';
+  api: VgAPI;
+  flag1 = false;
+  flag2 = false;
   constructor() {}
-  onPlayerReady(api:VgAPI) {
+  onPlayerReady(api: VgAPI) {
+    let time = 0;
     this.api = api;
     this.api.play();
-    this.api.seekTime().subscriptions.currentTime.subscribe(()=> {
-      console.log(this.api.getDefaultMedia().currentTime)
-    })
-    this.api.getDefaultMedia().subscriptions.ended.subscribe(
-      () => {
-        // Set the video to the beginning
-        this.api.getDefaultMedia().currentTime = 0;
+    this.api.getDefaultMedia().subscriptions.timeUpdate.subscribe(() => {
+      time++;
+      if (this.api.getDefaultMedia().currentTime > 5 && !this.flag1) {
+        this.api.pause();
+        alert('啊呀～下雨啦');
+        this.flag1 = true;
       }
-    );
+      if (this.api.getDefaultMedia().currentTime > 15 && !this.flag2) {
+        this.api.pause();
+        alert('终于到了');
+        this.flag2 = true;
+      }
+      console.log(this.api.getDefaultMedia().currentTime);
+    });
+    this.api.getDefaultMedia().subscriptions.ended.subscribe(() => {
+      // Set the video to the beginning
+      this.api.getDefaultMedia().currentTime = 0;
+    });
+  }
+  continue() {
+    this.api.play();
   }
 }
