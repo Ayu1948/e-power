c262b1074cd2daebb22fa57fd169fa1fc6b8ffe8
zht <Ayu1948@163.com>
2019-07-10 14:01:01 +0800

    完成视频界面适配和修正部分按钮

--- src/app/scene/scene.page.ts
+++ src/app/scene/scene.page.ts
@@ -1,4 +1,3 @@
-// import { VideoPlayer } from '@ionic-native/video-player/ngx';
 import { Component } from '@angular/core';
 import { VgAPI } from 'videogular2/core';
 
@@ -8,35 +7,43 @@ import { VgAPI } from 'videogular2/core';
   styleUrls: ['scene.page.scss']
 })
 export class ScenePage {
-  preload = 'auto';
   api: VgAPI;
-  flag1 = false;
-  flag2 = false;
+  showBtn = false;
+  skipBtn = false;
   constructor() {}
   onPlayerReady(api: VgAPI) {
-    let time = 0;
+    let flag1 = false;
+    let flag2 = false;
     this.api = api;
-    this.api.play();
+    // this.api.play();
     this.api.getDefaultMedia().subscriptions.timeUpdate.subscribe(() => {
-      time++;
-      if (this.api.getDefaultMedia().currentTime > 5 && !this.flag1) {
-        this.api.pause();
-        alert('啊呀～下雨啦');
-        this.flag1 = true;
+      if (!this.skipBtn) {
+        if (this.api.getDefaultMedia().currentTime > 5 && !flag1) {
+          this.api.pause();
+          alert('啊呀～下雨啦');
+          flag1 = true;
+          this.showBtn = true;
+        }
+        if (this.api.getDefaultMedia().currentTime > 15 && !flag2) {
+          this.api.pause();
+          alert('终于到了');
+          flag2 = true;
+          this.showBtn = true;
+        }
       }
-      if (this.api.getDefaultMedia().currentTime > 15 && !this.flag2) {
-        this.api.pause();
-        alert('终于到了');
-        this.flag2 = true;
-      }
-      console.log(this.api.getDefaultMedia().currentTime);
     });
     this.api.getDefaultMedia().subscriptions.ended.subscribe(() => {
+      alert('终于到我了');
       // Set the video to the beginning
-      this.api.getDefaultMedia().currentTime = 0;
+      // this.api.getDefaultMedia().currentTime = 0;
     });
   }
   continue() {
     this.api.play();
+    this.showBtn = false;
+  }
+  skip() {
+    this.api.getDefaultMedia().currentTime = 100;
+    this.skipBtn = true;
   }
 }
